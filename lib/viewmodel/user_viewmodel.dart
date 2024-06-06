0d97ab4d8d829ea37dc485014a0d93b5137f09a5
Alejandro Botella <abotellacosta2@gmail.com>
2024-06-07 01:35:26 +0200

    Esperemos que última modificación

--- lib/viewmodel/user_viewmodel.dart
+++ lib/viewmodel/user_viewmodel.dart
@@ -134,7 +134,7 @@ class UserViewModel {
   }
 
   static bool isValidEmail(String email) {
-    final regex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+    final regex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
     return regex.hasMatch(email);
   }
 
